b886d9cb90f153a9057dc6abac2178fdb0d6af5f
Andres0919 <andres.posada0919@gmail.com>
2018-09-30 17:59:45 -0500

    title changed

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Test Likeu</title>
         <link rel="stylesheet" href="{{ asset('plugins/bootstrap/css/bootstrap.css') }}">
         <link rel="stylesheet" href="{{ asset('css/customs.css') }}">
         <!-- Fonts -->
